1f7357d51157a3fd36f5717bc6a43a9b58a1dded
wyg_mac <aysfwyg@163.com>
2016-10-10 17:42:27 +0800

    Initial Commit

---
+++ YGAddressBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YGAddressBook
+//
+//  Created by wyg_mac on 16/10/10.
+//  Copyright © 2016年 wangyaoguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YGAddressBook/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YGAddressBook
+//
+//  Created by wyg_mac on 16/10/10.
+//  Copyright © 2016年 wangyaoguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
